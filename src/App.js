061349819ca2848f6e3840243c6443a705655fc8
varaprasadh <varaprasadh.a@gmail.com>
2019-09-21 00:49:57 +0530

    initial setup files configured

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         i will make fun of everything.😙 
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
