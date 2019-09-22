e7a75a8251cde6323fa54da04218db9a6ae092ae
varaprasadh <varaprasadh.a@gmail.com>
2019-09-23 00:04:52 +0530

    config done

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import Router from './Router';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
